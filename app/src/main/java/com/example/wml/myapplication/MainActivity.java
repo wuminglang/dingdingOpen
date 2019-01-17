9701a0872373726e6978753e3fa722a2941d444d
wuminglang <325802ssb>
2019-01-17 16:03:52 +0800

    锁屏自动开启

--- app/src/main/java/com/example/wml/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wml/myapplication/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.wml.myapplication;
 
+import android.app.KeyguardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -17,16 +19,19 @@ import java.util.Date;
 import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    KeyguardManager keyguardManager;
+    PowerManager pm;
+    PowerManager.WakeLock wl;
+    KeyguardManager.KeyguardLock keyguardLock;
+    PackageManager packageManager;
+    String packageName = "com.alibaba.android.rimet";//要打开应用的包名,以钉钉为例
     private Timer timer = new Timer(true);
     private Button btn;
-    private EditText start ;
+    private EditText start;
     private String setTime;
+    private Handler handler;
 
     private TimerTask task1 = new TimerTask() {
         public void run() {
@@ -36,17 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     };
 
-
-    private TimerTask task2 = new TimerTask() {
-        public void run() {
-            Message msg = new Message();
-            msg.what = 2;
-            handler.sendMessage(msg);
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -54,20 +51,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn.setOnClickListener(this);
 
         start = (EditText) findViewById(R.id.start);
+
+        packageManager = getPackageManager();
+        keyguardManager = (KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE);
+        pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wl = pm.newWakeLock(PowerManager.ACQUIRE_CAUSES_WAKEUP
+                | PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "bright");
+        keyguardLock = keyguardManager.newKeyguardLock("unLock");
+
+        handler = new Handler() {
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                if (msg.what == 1) {
+                    System.out.println("进入任务完成");
+                    SimpleDateFormat df = new SimpleDateFormat("HHmmss");//设置日期格式
+                    String dateTime = df.format(new Date());
+                    System.out.println("当前检查时间：" + dateTime);
+                    System.out.println("setTime：" + setTime);
+                    if (dateTime.startsWith(setTime)) {
+                        System.out.println("准备解锁屏幕");
+                        wakeUpAndUnlock();
+                        System.out.println("准备启动任务");
+                        Intent launchIntentForPackage = packageManager.getLaunchIntentForPackage(packageName);
+                        if (launchIntentForPackage != null)
+                            startActivity(launchIntentForPackage);
+                        else
+                            Toast.makeText(MainActivity.this, "手机未安装该应用", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        };
     }
 
 
     @Override
     public void onClick(View v) {
         setTime = start.getText().toString();
-        if(setTime.length()<=2){
+        if (setTime.length() <= 2) {
             Random rand = new Random();
             int randNumber = rand.nextInt(59 - 40 + 1) + 40;
-            setTime = setTime+String.valueOf(randNumber);
-            System.out.println("默认随机setTime"+setTime);
+            setTime = setTime + String.valueOf(randNumber);
+            System.out.println("默认随机setTime" + setTime);
         }
         //timer.schedule(task1, 0, 20*60*1000);//间隔 20 分钟
-        if(task1 !=null){
+        if (task1 != null) {
             task1.cancel();
             task1 = new TimerTask() {
                 public void run() {
@@ -77,32 +104,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             };
         }
-        timer.schedule(task1, 0, 60*1000);//间隔 60秒
-        System.out.println("设置完成setTime"+setTime);
+        timer.schedule(task1, 0, 60 * 1000);//间隔 60秒
+        System.out.println("设置完成setTime" + setTime);
         Toast.makeText(MainActivity.this, "设置完成", Toast.LENGTH_SHORT).show();
     }
 
-    private Handler handler  = new Handler(){
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-             if(msg.what == 1){
-                 System.out.println("进入任务完成");
-                SimpleDateFormat df = new SimpleDateFormat("HHmmss");//设置日期格式
-                String dateTime =df.format(new Date());
-                System.out.println("当前检查时间："+dateTime);
-                 System.out.println("setTime："+setTime);
-                if(dateTime.startsWith(setTime)){
-                    System.out.println("准备启动任务");
-                    PackageManager packageManager = getPackageManager();
-                    String packageName = "com.alibaba.android.rimet";//要打开应用的包名,以钉钉为例
-                    // String packageName = "com.tencent.mm";//要打开应用的包名,以微信为例
-                    Intent launchIntentForPackage = packageManager.getLaunchIntentForPackage(packageName);
-                    if (launchIntentForPackage != null)
-                        startActivity(launchIntentForPackage);
-                    else
-                        Toast.makeText(MainActivity.this, "手机未安装该应用", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-    };
+
+
+
+    public void wakeUpAndUnlock() { // 获取电源管理器对象
+        if (!pm.isScreenOn()) {
+            // 获取PowerManager.WakeLock对象,后面的参数|表示同时传入两个值,最后的是LogCat里用的Tag
+            wl.acquire(10000);
+            // 点亮屏幕
+            wl.release(); // 释放
+        }
+        //keyguardLock.reenableKeyguard();
+        keyguardLock.disableKeyguard(); // 解锁
+
+    }
 }
